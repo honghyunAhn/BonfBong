0127df37758a2ea2d3f3f81dd91a479d92ae5f40
honghyunAhn <anh2073@naver.com>
2021-07-13 13:32:11 +0900

    [2021-07-13]안홍현 : Project Start

---
+++ src/main/java/anjyu/bongbong/BongBongApplication.java
@@ -0,0 +1,13 @@
+package anjyu.bongbong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BongBongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BongBongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/anjyu/bongbong/BongBongApplicationTests.java
@@ -0,0 +1,13 @@
+package anjyu.bongbong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BongBongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
